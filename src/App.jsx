480c80df011258553f6dd5f46956079f11abfec3
SdtElectronics <meiyang423@Gmail.com>
2022-10-04 22:24:38 -0500

    Use react-query to fetch course list
    * add fetch.js
    * use react-query to fetch course list

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,34 @@
 import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 import Banner from './components/banner';
 import CourseList from './components/courseList'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+const queryClient = new QueryClient();
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
+
+  if(error)      return <h1>Error loading the course list: {`${error}`}</h1>;
+  if(isLoading)  return <h1>Loading...</h1>;
+  if(!data)      return <h1>Failed to find the course list</h1>;
 
   return (
     <div>
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses}/>
+      <Banner title={data.title} />
+      <CourseList courses={data.courses}/>
     </div>
+  );
+}
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient} contextSharing={true}>
+      <Main/>
+    </QueryClientProvider>
   );
 };
 
