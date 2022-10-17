a55a9085bf94e0034400dd38cc84203c5bd4fc5b
SdtElectronics <meiyang423@Gmail.com>
2022-10-16 22:23:36 -0500

    Add course edit form validation

--- src/components/editForm.jsx
+++ src/components/editForm.jsx
@@ -4,7 +4,7 @@ import { useFormData } from '../utilities/useFormData';
 const InputField = ({name, text, state, change}) => (
   <div className="mb-3">
     <label htmlFor={name} className="form-label">{text}</label>
-    <input className="form-control" id={name} name={name} 
+    <input className={`form-control ${state.errors ? "is-invalid" : ""}`} id={name} name={name} 
       defaultValue={state.values?.[name]} onChange={change} />
     <div className="invalid-feedback">{state.errors?.[name]}</div>
   </div>
@@ -21,16 +21,22 @@ const ButtonBar = ({message, disabled}) => {
   );
 };
 
+const validateTitle = (key, val) => /(^\w\w)/.test(val) ? '' : 'Title must be at least two characters';
+
+const validateMeets = (key, val) => 
+/^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/.test(val) ? '' : 'Meets must contain days and start-end, e.g., MWF 12:00-13:20';
+
 const EditForm = ({ courses }) => {
   const { id } = useParams();
-  const [state, change] = useFormData(courses[id]);
+  const [titleState, titleChange] = useFormData(validateTitle, courses[id]);
+  const [meetsState, meetsChange] = useFormData(validateMeets, courses[id]);
 
   return (
       <div className="container pt-3">
           <h2>Edit Course Information</h2>
           <form>
-              <InputField name="title" text="Title" state={state} change={change} />
-              <InputField name="meets" text="Meets" state={state} change={change} />
+              <InputField name="title" text="Title" state={titleState} change={titleChange} />
+              <InputField name="meets" text="Meets" state={meetsState} change={meetsChange} />
               <ButtonBar />
           </form>
       </div>
