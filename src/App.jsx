5bc36ded5241700da4cbda34ba945034eb9cfc2a
SdtElectronics <meiyang423@Gmail.com>
2022-10-18 20:29:45 -0500

    Fetch data from Firebase database

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch';
+import { useDbData } from "./utilities/firebase.js"
 import TermPage from './components/termPage';
 import EditForm from './components/editForm'
 import './App.css';
@@ -11,10 +12,9 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 const queryClient = new QueryClient();
 
 const Main = () => {
-  const [data, isLoading, error] = useJsonQuery("https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php");
+  const [data, error] = useDbData("/");
 
   if(error)      return <h1>Error loading the course list: {`${error}`}</h1>;
-  if(isLoading)  return <h1>Loading...</h1>;
   if(!data)      return <h1>Failed to find the course list</h1>;
 
   return (
---
+++ src/utilities/firebase.js
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getDatabase, onValue, ref, update } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_EwfiPd66iSvCus8BLkmw73Xwf2EEJfI",
+  authDomain: "cs397rps101.firebaseapp.com",
+  databaseURL: "https://cs397rps101-default-rtdb.firebaseio.com",
+  projectId: "cs397rps101",
+  storageBucket: "cs397rps101.appspot.com",
+  messagingSenderId: "273277845246",
+  appId: "1:273277845246:web:f6db9b0b5d271373bdefaf",
+  measurementId: "G-14XKCK95RS"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useDbData = (path) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => (
+    onValue(ref(database, path), (snapshot) => {
+     setData( snapshot.val() );
+    }, (error) => {
+      setError(error);
+    })
+  ), [ path ]);
+
+  return [ data, error ];
+};
+
+const makeResult = (error) => {
+  const timestamp = Date.now();
+  const message = error?.message || `Updated: ${new Date(timestamp).toLocaleString()}`;
+  return { timestamp, error, message };
+};
+
+export const useDbUpdate = (path) => {
+  const [result, setResult] = useState();
+  const updateData = useCallback((value) => {
+    update(ref(database, path), value)
+    .then(() => setResult(makeResult()))
+    .catch((error) => setResult(makeResult(error)))
+  }, [database, path]);
+
+  return [updateData, result];
+};
